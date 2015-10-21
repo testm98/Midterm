c32c1bf837ec2d2891280211778c48700a7e744a
bam59cst112day <bam59cst112day@suffolk.li>
2015-10-21 14:14:03 -0400

    Update classes.java

--- classes.java
+++ classes.java
@@ -13,5 +13,16 @@ class Ball()
   void show() {
     fill(r,g,b);
     ellipse( x,y, 30,30 );
-    }
+  }
+  void move() {
+    x=  x+dx;
+    y=  y+dy;
+  }
+  // Detect if this Ball has hit abother Ball. 
+  boolean hit( Ball other ) {
+    if ( dist( this.x,this.y, other.x,other.y ) < 60 ) return true;
+    else return false;
+  }
+  
+  
   
